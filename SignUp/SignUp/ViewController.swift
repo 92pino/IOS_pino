38b79ce0c6e4bb239befc0427385a43f111e9e76
JinBae Jeong <jinbaejeong@JinBaeui-MacBookPro.local>
2018-11-22 23:57:23 +0900

    signUp project init

---
+++ SignUp/SignUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignUp
+//
+//  Created by JinBae Jeong on 22/11/2018.
+//  Copyright © 2018 JinBae Jeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
