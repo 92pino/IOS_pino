2c782a321e62d41de0d6d30388e4687e7d842989
Pino <jjb8382@gmail.com>
2018-11-26 17:23:15 +0900

    ViewTransition add

---
+++ ViewTransition/ViewTransition/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ViewTransition
+//
+//  Created by JinBae Jeong on 26/11/2018.
+//  Copyright © 2018 JinBae Jeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ViewController의 view가 메모리에 로드됨")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+    }
+
+}
+
