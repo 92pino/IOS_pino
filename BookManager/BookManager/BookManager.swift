bc7e3570a61994935fe539607f47ae7c85c5220b
92pino <jjb8382@gmail.com>
2018-11-28 20:28:09 +0900

    도서관리 func add

---
+++ BookManager/BookManager/BookManager.swift
@@ -0,0 +1,38 @@
+//
+//  BookManager.swift
+//  BookManager
+//
+//  Created by JinBae Jeong on 28/11/2018.
+//  Copyright © 2018 JinBae Jeong. All rights reserved.
+//
+
+import Foundation
+
+class BookManager {
+    var bookList = [Book]()
+    
+    // 책 등록
+    func registerBook(bookObject:Book) {
+        
+    }
+    
+    // 전체 책 리스트 show
+//    func showAllBooks() -> String {
+//
+//    }
+    
+    // 책 갯수 return
+//    func countBooks() -> Int {
+//
+//    }
+    
+    // 책 검색
+//    func searchBook(name:String) -> String?{
+//
+//    }
+    
+    // 책 삭제
+//    func removeBook(name:String){
+//
+//    }
+}
