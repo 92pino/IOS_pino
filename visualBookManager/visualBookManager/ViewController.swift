dc3af62fa63ecd6c34d81228920be1b13a85f8ce
92pino <jjb8382@gmail.com>
2018-11-28 22:59:15 +0900

    visualBookManage init

---
+++ visualBookManager/visualBookManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  visualBookManager
+//
+//  Created by JinBae Jeong on 28/11/2018.
+//  Copyright © 2018 JinBae Jeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
