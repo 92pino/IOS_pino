f41d8a82278410cb95d5c7feecb9453b63c434ee
92pino <jjb8382@gmail.com>
2019-04-02 17:14:44 +0900

    20190402 과제세팅

---
+++ 과제/20190402/ViewControllerTransition/ViewControllerTransition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewControllerTransition
+//
+//  Created by JinBae Jeong on 02/04/2019.
+//  Copyright © 2019 pino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 과제/20190402/typeCasting.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+/*
+
+ [ 과제 ]
+    1.
+        func addTwoValues(a: Int, b: Int) -> Int {
+        return a + b
+        }
+        let task1: Any = addTwoValues(a: 2, b: 3)
+        위와 같이 task1 이라는 변수가 있을 때
+        task1 + task1 의 결과가 제대로 출력되도록 할 것
+ 
+ [ 도전과제 ]
+    1.
+         let task2: Any = addTwoValues
+         위와 같이 task2 라는 변수가 있을 때
+         task2 + task2 의 결과가 제대로 출력되도록 할 것 (addTwoValues의 각 파라미터에는 2와 3 입력)
+ 
+ 
+*/
