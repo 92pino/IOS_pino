dccba38c85de2800233a2123c888f2df1dcde2aa
92pino <jjb8382@gmail.com>
2019-04-08 00:00:06 +0900

    delegate-textfield add

--- Practice/Delegate-TextField/Delegate-TextField/ViewController.swift
+++ Practice/Delegate-TextField/Delegate-TextField/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// UITextFieldDelegate라는 델리게이트 프로토콜 선언
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var tf: UITextField!
     
@@ -34,8 +35,64 @@ class ViewController: UIViewController {
         tf.layer.borderWidth = 2.0
         
         tf.becomeFirstResponder()
+        
+        // 델리게이트 지정
+        self.tf.delegate = self
+        
     }
 
-
+    @IBAction func confirm(_ sender: Any) {
+        // 텍스트 필드를 최초 응답자 객체에서 해제
+        self.tf.resignFirstResponder()
+    }
+    
+    @IBAction func input(_ sender: Any) {
+        // 텍스트 필드를 최초 응답자 객체로 지정
+        self.tf.becomeFirstResponder()
+    }
+    
+    // 텍스트필드의 편집을 시작할 때 호출
+    func textFieldShouldBeginEditing(_ textField: UITextField) {
+        print("텍스트 필드의 편집이 시작됩니다.")
+    }
+    
+    // 텍스트 필드의 내용이 삭제될 때 호출
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print("텍스트 필드의 내용이 삭제됩니다")
+        return true
+    }
+    
+    // 텍스트 필드의 내용이 변경될 때 호출
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print("텍스트 필드의 내용이 \(string)으로 변경됩니다.")
+        if Int(string) == nil { // 입력된 값이 숫자가 아니라면 true를 리턴
+            if (textField.text?.count)! + string.count > 10 {
+                return false
+            } else {
+                return true
+            }
+        } else {    // 입력된 값이 숫자라면 false를 리턴
+            return false
+        }
+        
+    }
+    
+    // 텍스트 필드의 리턴키가 눌러졌을 때 호출
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        print("텍스트 필드의 리턴키가 눌러졌습니다.")
+        return true
+    }
+    
+    // 텍스트 필드 편집이 종료될 때 호출
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print("텍스트 필드의 편집이 종료됩니다.")
+        return true
+    }
+    
+    // 텍스트 필드의 편집이 종료되었을 때 호출
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("텍스트 필드의 편집이 종료되었습니다.")
+    }
 }
 
