cd282430fa4d6dd61e24d6d8d450a60bcc202b3f
92pino <jjb8382@gmail.com>
2019-04-09 12:24:16 +0900

    autoLayout example

---
+++ Class/20190409-AutoLayoutExample/20190409-AutoLayoutExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20190409-AutoLayoutExample
+//
+//  Created by JinBae Jeong on 09/04/2019.
+//  Copyright © 2019 pino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
