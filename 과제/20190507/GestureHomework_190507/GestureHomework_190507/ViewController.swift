d3732a0af71ef943d98f14b7d138ce3710e325a3
92pino <jjb8382@gmail.com>
2019-05-07 16:44:23 +0900

    20190507 도전과제 init

---
+++ 과제/20190507/GestureHomework_190507/GestureHomework_190507/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GestureHomework_190507
+//
+//  Created by JinBae Jeong on 07/05/2019.
+//  Copyright © 2019 pino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
