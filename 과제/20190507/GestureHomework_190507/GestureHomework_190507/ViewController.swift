3f8cee3096526e02978e649db541dd29745f87cf
92pino <jjb8382@gmail.com>
2019-05-07 20:05:54 +0900

    20190507 tapGesture homework complelte

--- 과제/20190507/GestureHomework_190507/GestureHomework_190507/ViewController.swift
+++ 과제/20190507/GestureHomework_190507/GestureHomework_190507/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var countText: UILabel!
+  @IBOutlet weak var position: UILabel!
+  
+  var startFlag = CGPoint(x: 0, y: 0)
+  var count: Int = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    tapGestureSetting()
+  }
+  
+  func tapGestureSetting() {
+    let touchGesture = UITapGestureRecognizer(target: self, action: #selector(tapGesture(_:)))
+    touchGesture.numberOfTapsRequired = 1
+    touchGesture.numberOfTouchesRequired = 1
+    view.addGestureRecognizer(touchGesture)
   }
 
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesBegan(touches, with: event)
+    
+    guard let touch = touches.first else { return }
+    // 어느 위치를 찍었는지에 대한 좌표 정보
+    let touchPoint = touch.location(in: touch.view)
+    
+  }
+  
+  @objc func tapGesture(_ sender: UITapGestureRecognizer){
+    let touchPoint = sender.location(in: self.view)
+    
+    if abs(startFlag.x - touchPoint.x) >= 10 || abs(startFlag.y - touchPoint.y) >= 10 {
+      count = 0
+      print("초기화", abs(startFlag.x - touchPoint.x), abs(startFlag.y - touchPoint.y))
+    }
+//
+    if count == 0 {
+      startFlag = touchPoint
+    }
+    
+    count += 1
+//
+    countText.text = "\(count)"
+    position.text = "좌표 : (\(String(format: "%.3f", arguments: [touchPoint.x])), \(String(format: "%.3f", arguments: [touchPoint.y])))"
+    
+    print(count)
+    
+  }
 
 }
-
