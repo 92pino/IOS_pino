2c1512f898284458a5a2de28a78d5b859148df0e
92pino <jjb8382@gmail.com>
2019-05-07 16:41:15 +0900

    20190507 수업내용 upload & 과제 1번 upload

--- 수업자료/20190507/GestureRecognizerExample - Starter/GestureRecognizerExample/TouchViewController.swift
+++ 수업자료/20190507/GestureRecognizerExample - Starter/GestureRecognizerExample/TouchViewController.swift
@@ -11,9 +11,87 @@ import UIKit
 final class TouchViewController: UIViewController {
 
   @IBOutlet private weak var imageView: UIImageView!
+  var isHoldingImage = false
+  var diffx: CGFloat = 0
+  var diffy: CGFloat = 0
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    styleImageView()
+  }
+  
+  // 이미지뷰 layer 기능
+  func styleImageView(){
+    imageView.layer.cornerRadius = imageView.frame.width / 2
+    
+    // 이미지뷰를 벗어나는 영역은 잘라버리는 기능
+    // 1. maskToBounds
+    //    imageView.layer.masksToBounds = true
+    
+    // 2. clipsToBounds
+    imageView.clipsToBounds = true
+  }
+  
+  // 터치를 인식하는 시점
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesBegan(touches, with: event)
+    
+    // 터치한 곳의 정보가 들어옴
+    // print(touches)
+    
+    // 손가락중 제일 먼저 반응한것
+    guard let touch = touches.first else { return }
+    // 어느 위치를 찍었는지에 대한 좌표 정보
+    let touchPoint = touch.location(in: touch.view)
+    
+    if imageView.frame.contains(touchPoint) {
+      imageView.image = UIImage(named: "cat2")
+      isHoldingImage = true
+    }
+    
+    diffx = touchPoint.x - imageView.frame.origin.x
+    diffy = touchPoint.y - imageView.frame.origin.y
+  }
+  
+  // 터치 후 움직일때 시점
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesMoved(touches, with: event)
+    
+    // 손가락중 제일 먼저 반응한것
+    guard isHoldingImage, let touch = touches.first else { return }
+    // 어느 위치를 찍었는지에 대한 좌표 정보
+    let touchPoint = touch.location(in: touch.view)
+    
+    imageView.frame.origin.x = touchPoint.x - diffx
+    imageView.frame.origin.y = touchPoint.y - diffy
+    
+    
+  }
+  
+  // 터치를 뗏을때 시점
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesEnded(touches, with: event)
+    
+    // 손가락중 제일 먼저 반응한것
+    guard let touch = touches.first else { return }
+    // 어느 위치를 찍었는지에 대한 좌표 정보
+    let touchPoint = touch.location(in: touch.view)
+    
+    isHoldingImage = false
+    imageView.image = UIImage(named: "cat1")
+  }
+  
+  // 터치를 하고 있다가 다른 상황에 의해 방해 받을때 생기는 시점
+  override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    // 손가락중 제일 먼저 반응한것
+    guard let touch = touches.first else { return }
+    // 어느 위치를 찍었는지에 대한 좌표 정보
+    let touchPoint = touch.location(in: touch.view)
+    
+    isHoldingImage = false
+    imageView.image = UIImage(named: "cat1")
   }
   
 }
--- 수업자료/20190507/ImagePickerControllerExample (Starter)/ImagePickerControllerExample/ViewController.swift
+++ 수업자료/20190507/ImagePickerControllerExample (Starter)/ImagePickerControllerExample/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019년 giftbot. All rights reserved.
 //
 
+import MobileCoreServices
 import UIKit
 
 final class ViewController: UIViewController {
@@ -15,28 +16,120 @@ final class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    imagePickerController.delegate = self
   }
   
   @IBAction private func pickImage(_ sender: Any) {
     print("\n---------- [ pickImage ] ----------\n")
+    present(imagePickerController, animated: true)
+    
+    // 앨범 타입
+    imagePickerController.sourceType = .photoLibrary
+//    imagePickerController.sourceType = .savedPhotosAlbum
   }
   
   
   @IBAction private func takePicture(_ sender: Any) {
     print("\n---------- [ takePicture ] ----------\n")
+    guard UIImagePickerController.isSourceTypeAvailable(.camera) else { return }
+    imagePickerController.sourceType = .camera
+
+    imagePickerController.mediaTypes = [kUTTypeImage as String]
+//    imagePickerController.mediaTypes = ["public.image"]
+    
+//    let mediaTypes = UIImagePickerController.availableMediaTypes(for: .camera)
+//    imagePickerController.mediaTypes = mediaTypes ?? []
+    
+    present(imagePickerController, animated: true)
   }
   
   @IBAction private func takePictureWithDelay(_ sender: Any) {
     print("\n---------- [ takePictureWithDelay ] ----------\n")
+    
+    guard UIImagePickerController.isSourceTypeAvailable(.camera) else { return }
+    imagePickerController.sourceType = .camera
+    imagePickerController.mediaTypes = [kUTTypeImage as String]
+    present(imagePickerController, animated: true) {
+//      self.imagePickerController.takePicture()
+      
+      // 타이머를 주고싶다면 + 2 를 변경 (파라미터값으로 받아오기)
+      DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+        self.imagePickerController.takePicture()
+      })
+    }
   }
   
   
   @IBAction private func recordingVideo(_ sender: Any) {
     print("\n---------- [ recordingVideo ] ----------\n")
+    
+    guard UIImagePickerController.isSourceTypeAvailable(.camera) else { return }
+    imagePickerController.sourceType = .camera
+    // kUTTypeMovie : 오디오가 포함된 동영상
+    // kUTTypeVideo : 오디오가 포함되지 않은 동영상
+    imagePickerController.mediaTypes = [kUTTypeMovie as String]
+    // imagePickerController.mediaTypes = ["public.movie"]
+    
+    // 앞쪽 카메라
+    // imagePickerController.cameraDevice = .front
+    
+    // 뒤쪽 카메라
+    // imagePickerController.cameraDevice = .rear
+    
+    // 카메라, 비디오 두가지 다 사용 가능하게 하기
+    let mediaTypes = UIImagePickerController.availableMediaTypes(for: .camera)
+    imagePickerController.mediaTypes = mediaTypes ?? []
+    
+    // 어떤거를 먼저 시작 화면으로 만들지
+    // imagePickerController.cameraCaptureMode = .video
+    imagePickerController.cameraCaptureMode = .photo
+    
+    present(imagePickerController, animated: true)
+    
+    // imagePickerController.startVideoCapture()
+    // imagePickerController.stopVideoCapture()
   }
 
   @IBAction private func toggleAllowsEditing(_ sender: Any) {
     print("\n---------- [ toggleAllowsEditing ] ----------\n")
+    imagePickerController.allowsEditing.toggle()
   }
 }
 
+// MARK - UIImagePickerControllerDelegate
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+  //
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    picker.dismiss(animated: true)
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    print("\n---------- [ didFinishPickingMediaWithInfo ] ----------\n")
+    
+    let mediaType = info[.mediaType] as! NSString
+    if UTTypeEqual(mediaType, kUTTypeImage) {
+      let originalImage = info[.originalImage] as! UIImage
+      let editedImage = info[.editedImage] as? UIImage
+      let selectedImage = editedImage ?? originalImage
+      
+      if picker.sourceType == .camera {
+      // 사진 저장
+        UIImageWriteToSavedPhotosAlbum(selectedImage, nil, nil, nil)
+      }
+      
+      imageView.image = selectedImage
+    } else if UTTypeEqual(mediaType, kUTTypeMovie) {
+      print("Video info :", info)
+      if let mediaPath = (info[.mediaURL] as? NSURL)?.path,
+        UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(mediaPath) {
+        UISaveVideoAtPathToSavedPhotosAlbum(mediaPath, nil, nil, nil)
+      }
+    }
+    
+    picker.dismiss(animated: true)
+  }
+  
+  
+}
