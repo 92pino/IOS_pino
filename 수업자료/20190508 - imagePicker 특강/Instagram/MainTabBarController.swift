e6a5577cacc700a261bffdfd5bf400082aa1c8d0
92pino <jjb8382@gmail.com>
2019-05-08 15:57:56 +0900

    Instagram practice

---
+++ 수업자료/20190508 - imagePicker 특강/Instagram/MainTabBarController.swift
@@ -0,0 +1,56 @@
+//
+//  MainTabBarController.swift
+//  Instagram
+//
+//  Created by JinBae Jeong on 08/05/2019.
+//  Copyright © 2019 Wi. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    let homeVC = HomeViewController()
+    let plusVC = PlusViewController()
+    let myVC = MyViewController()
+    let picker = UIImagePickerController()
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        picker.delegate = self
+
+        // 탭바 위에 뷰컨트롤러 올리기
+      
+        homeVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: AppImageData.homeNomal), selectedImage: UIImage(named: AppImageData.homeSelected))
+        plusVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: AppImageData.plus), selectedImage: UIImage(named: AppImageData.plus))
+      
+        // homeVC에 네비게이션 적용시키기
+        viewControllers = [UINavigationController.init(rootViewController: homeVC), plusVC, UINavigationController(rootViewController: myVC)]
+      
+      self.delegate = self
+      
+      // 탭바 타이틀 공간 없애기
+      for i in tabBar.items! {
+        i.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+      }
+    }
+  
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+      if viewController == plusVC {
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+        return false
+      }
+      
+      return true
+    }
+
+}
+
+extension MainTabBarController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    NotificationCenter.default.post(name: NSNotification.Name("photoSelected"), object: nil, userInfo: ["image":info[.originalImage] as Any])
+    picker.dismiss(animated: true, completion: nil)
+  }
+}
