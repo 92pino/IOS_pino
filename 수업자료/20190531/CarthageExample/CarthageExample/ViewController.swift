90269538912a66adacededd7a1305fa0558e446b
92pino <jjb8382@gmail.com>
2019-05-31 16:53:08 +0900

    carthageExample upload

--- 수업자료/20190531/CarthageExample/CarthageExample/ViewController.swift
+++ 수업자료/20190531/CarthageExample/CarthageExample/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import DynamicColor
 
 class ViewController: UIViewController {
 
+    @IBOutlet var circles: [UIView]!
+    
+    @IBOutlet var circle1: UIView!
+    @IBOutlet var circle2: UIView!
+    @IBOutlet var circle3: UIView!
+    @IBOutlet var circle4: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        circles.forEach {
+            $0.layer.cornerRadius = 40
+        }
+        
+        var circles2 = [circle1, circle2, circle3, circle4].forEach {
+            $0.layer.cornerRadius = 40
+        }
+        
+        let blue   = UIColor(hexString: "#3498db")
+        let red    = UIColor(hexString: "#e74c3c")
+        let yellow = UIColor(hexString: "#f1c40f")
+        
+        let gradient = DynamicGradient(colors: [blue, red, yellow])
+        let labPalette = gradient.colorPalette(amount: 8, inColorSpace: .lab)
+        circles.enumerated().forEach { $1.backgroundColor = labPalette[$0] }
+        
+        let color = DynamicColor(hexString: "#c0392b")
+        circle1.backgroundColor = color
+        circle2.backgroundColor = color.darkened()
+        circle3.backgroundColor = color.inverted()
+        
+        let mixedColor = color.mixed(withColor: .white)
+        circle4.backgroundColor = mixedColor
     }
 
 
