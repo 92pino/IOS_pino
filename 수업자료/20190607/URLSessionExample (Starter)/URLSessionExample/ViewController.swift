2e3ff3d69ace98d059a8746ced8a93fcaa56e8cc
92pino <jjb8382@gmail.com>
2019-06-07 11:20:07 +0900

    20190607 수업자료 update

---
+++ 수업자료/20190607/URLSessionExample (Starter)/URLSessionExample/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  URLSessionExample
+//
+//  Created by giftbot on 2019. 6. 6..
+//  Copyright © 2019년 giftbot. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+  
+  @IBOutlet private weak var imageView: UIImageView!
+  private let imageUrlStr = "https://loremflickr.com/860/640/cat"
+  
+  
+  // MARK: - Sync method
+  
+  @IBAction private func syncMethod() {
+    print("\n---------- [ syncMethod ] ----------\n")
+    
+  }
+  
+  
+  // MARK: - URLComponents
+  
+  @IBAction private func urlComponentsExample(_ sender: Any) {
+    print("\n---------- [ urlComponentsExample ] ----------\n")
+    /***************************************************
+     http://username:password@www.example.com:80/index.html?key1=value1&key2=value2#frag
+     ***************************************************/
+
+  }
+  
+  
+  // MARK: - URL Encoding Example
+  
+  @IBAction private func urlEncodingExample() {
+    print("\n---------- [ urlEncodingExample ] ----------\n")
+    
+  }
+  
+  
+  
+  // MARK: - Session Configuration
+  
+  @IBAction private func sessionConfig(_ sender: Any) {
+    print("\n---------- [ Session Configuration ] ----------\n")
+
+  }
+  
+
+  // MARK: - Get, Post, Delete
+  
+  @IBAction func requestGet(_ sender: Any) {
+    print("\n---------- [ Get Method ] ----------\n")
+    let todoEndpoint = "https://jsonplaceholder.typicode.com/todos/1"
+  }
+  
+  
+  @IBAction func requestPost(_ sender: Any) {
+    print("\n---------- [ Post Method ] ----------\n")
+    let todoEndpoint = "https://jsonplaceholder.typicode.com/todos"
+  }
+  
+  
+  @IBAction func requestDelete(_ sender: Any) {
+    print("\n---------- [ Delete Method ] ----------\n")
+    let todoEndpoint = "https://jsonplaceholder.typicode.com/todos/1"
+  }
+}
+
