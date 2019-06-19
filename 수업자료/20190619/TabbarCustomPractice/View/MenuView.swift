9eafa2dc5ac935f119f56607dc90415aaa5bd050
92pino <jjb8382@gmail.com>
2019-06-19 16:35:41 +0900

    CustomTabBar finish add

---
+++ 수업자료/20190619/TabbarCustomPractice/View/MenuView.swift
@@ -0,0 +1,21 @@
+//
+//  MenuView.swift
+//  TabbarCustomPractice
+//
+//  Created by JinBae Jeong on 19/06/2019.
+//  Copyright © 2019 pino. All rights reserved.
+//
+
+import UIKit
+
+class MenuView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
