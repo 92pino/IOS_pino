bdaa86a747e11307c1811bad459a2b4999a0872d
92pino <jjb8382@gmail.com>
2019-06-22 15:22:27 +0900

    1번 알고리즘 문제 finish

--- 수업자료/20190621/4차 시험 제출용.playground/Pages/1번 문제 (최종 결과).xcplaygroundpage/Contents.swift
+++ 수업자료/20190621/4차 시험 제출용.playground/Pages/1번 문제 (최종 결과).xcplaygroundpage/Contents.swift
@@ -6,10 +6,11 @@
 import Foundation
 
 func game (N: Int) -> String {
-    var winner = N % 2 == 1 ? "정아" : "창근"
+    var winner = [0, 2].contains(N % 7) ? "창근" : "정아"
     
     return winner
 }
+game(N: 0)
 game(N: 5)    // 승자 : 정아
 game(N: 19)   // 승자 : 정아
 game(N: 37)
